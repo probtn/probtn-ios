7adf79d123fc4cb18b1c42986f71c9c35043f809
vbaranov <baranov.viktor.27@gmail.com>
2015-06-22 22:54:29 +0300

    ProBtn Release 1.2.4 (tracking and click counter links support)

--- Library/ProBtnVersion.h
+++ Library/ProBtnVersion.h
@@ -9,11 +9,13 @@
 #define PROBTN_VERSION_1_0                      1000
 #define PROBTN_VERSION_1_1                      1100
 #define PROBTN_VERSION_1_2                      1200
+#define PROBTN_VERSION_1_3                      1300
+#define PROBTN_VERSION_1_4                      1400
 
 //----------------------------------------------//
 
 #ifndef PROBTN_VERSION_LATEST
-#   define PROBTN_VERSION_LATEST                PROBTN_VERSION_1_2
+#   define PROBTN_VERSION_LATEST                PROBTN_VERSION_1_4
 #endif
 
 //----------------------------------------------//
